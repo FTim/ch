d59e4b079d99cecb0afe6445fda3a42b7b59c4c0
Tim <faragotimike@gmail.com>
2018-04-16 07:11:52 +0200

    ReactionDemo - DocX generator inputs

--- ReactionDemo/WpfApp4/View/MainPage.xaml.cs
+++ ReactionDemo/WpfApp4/View/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using DocxGen;
 
 using WpfApp4.Model;
 
@@ -26,12 +27,17 @@ namespace WpfApp4.View
         public ObservableCollection<StartingMaterial> smaterialList { get; set; }
         public ObservableCollection<Reagent> reagentList { get; set; }
         public ObservableCollection<Solvent> solventList { get; set; }
+        public string ReactionImgPath;
+        public string ObservationImgPath;
+        public string ReportPath;
+        public DocxGenerator gen;
         
         public MainPage()
         {
             smaterialList = new ObservableCollection<StartingMaterial>();
             reagentList = new ObservableCollection<Reagent>();
             solventList = new ObservableCollection<Solvent>();
+            gen = new DocxGenerator();
             DataContext = this;
             InitializeComponent();
 
@@ -44,8 +50,8 @@ namespace WpfApp4.View
 
         private void btnSaveDocx_Click(object sender, RoutedEventArgs e)
         {
-            /*
-            DocxGenerator gen = new DocxGenerator();
+            
+           
             gen.ReactionCode = txtReactionCode.Text;
             gen.Chemist = txtChemist.Text;
             gen.Chiefchemist = txtChiefchemist.Text;
@@ -55,15 +61,48 @@ namespace WpfApp4.View
             gen.ClosureDate = datetimeClosure.DisplayDate;
             gen.PreviousStep = txtPreviousStep.Text;
             gen.Literature = txtLiterature.Text;
-            gen.ReactionImgPath = @"C:\Users\Timea\Desktop\img.jpg";
 
-            
-            gen.StartingMaterial = new MoleculeRow();
-            gen.StartingMaterial.Name = StartingMaterialGrid.ToString();
-            
-            GridViewRowPresenter sm_row = new GridViewRowPresenter();
-            */
-           
+            gen.ReactionImgPath = ReactionImgPath;
+            List<MoleculeRow> smMoleculeRow = new List<MoleculeRow>();
+            foreach (var item in smaterialList)
+            {
+                double? tmp_V;
+                if (item.VValue == null||item.VValue=="") tmp_V = null;
+                else tmp_V = Double.Parse(item.VValue);
+
+                double? tmp_m;
+                if (item.mValue == null || item.mValue == "") tmp_m = null;
+                else tmp_m = Double.Parse(item.mValue);
+
+                smMoleculeRow.Add(new MoleculeRow{ Name=item.Name, CAS=item.CAS, MWvalue=42, Vvalue=tmp_V, mvalue=tmp_m, nvalue=43, Denvalue=44, Mpvalue=45, Bpvalue=46, Ratio=47});
+            }
+
+            List<MoleculeRow> rMoleculeRow = new List<MoleculeRow>();
+            foreach (var item in reagentList)
+            {
+                
+
+                rMoleculeRow.Add(new MoleculeRow { Name = item.Name, CAS = item.CAS, MWvalue = 42, Vvalue = 43, mvalue = 44, nvalue = 43, Denvalue = 44, Mpvalue = 45, Bpvalue = 46, Ratio=Double.Parse(item.Ratio) });
+            }
+
+            List<MoleculeRow> sMoleculeRow = new List<MoleculeRow>();
+            foreach (var item in solventList)
+            {
+
+
+                smMoleculeRow.Add(new MoleculeRow { Name = item.Name, CAS = item.CAS, MWvalue = 42, Vvalue = Double.Parse(item.VValue), mvalue = 44, nvalue = 43, Denvalue = 44, Mpvalue = 45, Bpvalue = 46, Ratio = 47 });
+            }
+
+            gen.ProcedureText = txtProcedure.Text;
+            gen.Yield = txtYield.Text;
+            gen.ObservationText = txtObservation.Text;
+            List<string> obsimg = new List<string>();
+            obsimg.Add(ObservationImgPath);
+            gen.ObservationImg =obsimg;
+
+            gen.FilePath = ReportPath;
+            gen.GenerateReport();
+            MessageBox.Show("Done!");          
         }
 
         
@@ -71,7 +110,7 @@ namespace WpfApp4.View
         private void btnSAdd_Click(object sender, RoutedEventArgs e)
         {
             solventList.Add(new Solvent { Name = txtSAddName.Text, CAS = txtSAddCAS.Text, VValue = txtSAddVValue.Text });
-            //MessageBox.Show(solventList.ElementAt(0).Name);
+            
         }
 
         private void btnSMAdd_Click(object sender, RoutedEventArgs e)
@@ -83,5 +122,97 @@ namespace WpfApp4.View
         {
             reagentList.Add(new Reagent { Name = txtRAddName.Text, CAS = txtRAddCAS.Text, Ratio = txtRAddRatio.Text });
         }
+
+        private void btnAddRectionImg_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".jpg";
+            dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                ReactionImgPath = dlg.FileName;
+                MessageBox.Show("Reaction image selected: "+ReactionImgPath);
+            }
+            else MessageBox.Show("No image selected");
+        }
+
+        private void btnAddObservationImg_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+
+
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".jpg";
+            dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                ObservationImgPath = dlg.FileName;
+                MessageBox.Show("Observation image selected: " + ObservationImgPath);
+            }
+            else MessageBox.Show("No image selected");
+        }
+
+        private void btnSelectLocation_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+           // Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            Microsoft.Win32.SaveFileDialog sfd = new Microsoft.Win32.SaveFileDialog();
+
+
+
+            // Set filter for file extension and default file extension 
+            //dlg.DefaultExt = ".jpg";
+           // dlg.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg";
+
+            sfd.DefaultExt = ".docx";
+            sfd.Filter = "Docx Files (*.docx)| All files (*.*)";
+
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            //Nullable<bool> result = dlg.ShowDialog();
+
+            Nullable<bool> result2 = sfd.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result2 == true)
+            {
+                // Open document 
+                ReportPath = sfd.FileName;
+                MessageBox.Show("Report will be saved to: "+ReportPath);
+            }
+            else MessageBox.Show("No saving location given");
+        }
+
+        
+        private void btnHint_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
-}
+    }
+
